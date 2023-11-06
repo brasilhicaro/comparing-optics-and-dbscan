3c6fe8114c1218f08e4b7164503d966379870c08
Hicaro_Brasil <hicaro.brasil@hotmail.com>
2023-11-06 15:20:31 -0300

    :sparkles: feat: plotting results

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from dbscan import Dbscan
 import pandas as pd
 from optics import Optics
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     dbscan = Dbscan()
     optics = Optics()
     df_dbscan = dbscan.generate_csv()
+    df_dbscan = pd.read_csv('dbscan.csv')
+    print(dbscan.count_clusters())
     df_optics = optics.generate_csv()
+    df_optics = pd.read_csv('optics.csv')
+    print(optics.count_clusters())
+    
+    plt.scatter(df_dbscan['latitude'], df_dbscan['longitude'], c=df_dbscan['cluster'], cmap='Paired')
+    plt.show()    
+    plt.scatter(df_optics['latitude'], df_optics['longitude'], c=df_optics['cluster'], cmap='Paired')
+    plt.show()
+    
+    
 
