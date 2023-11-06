98e841983333e4af8c7fdde372170677ee841621
Hicaro_Brasil <hicaro.brasil@hotmail.com>
2023-11-06 09:45:41 -0300

    :sparkles: feat: finish dbscan class

--- dbscan.py
+++ dbscan.py
@@ -9,19 +9,20 @@ class Dbscan:
     __distance: float = 0.5 / 6371.0088
 
 
-    def __init__(self):
-        self.__data_firms = pd.read_csv('SUOMI_VIIRS_C2_South_America_24h.csv')
+    def __init__(self)->None:
+        self.__data_firms = pd.DataFrame(pd.read_csv(
+            'SUOMI_VIIRS_C2_South_America_24h.csv'))
         
-    def get_data_firms(self):
+    def get_data_firms(self)->pd.DataFrame:
         return self.__data_firms
     
-    def get_results(self):
+    def __get_results__(self)->pd.DataFrame:
         df = pd.DataFrame(self.__data_firms, columns=['latitude', 'longitude'])
 
         df = df.dropna()
 
         dbscan = DBSCAN(
-                    eps== self.__distance,
+                    eps=self.__distance,
                     min_samples=1,
                     algorithm='ball_tree',
                     metric='haversine'
@@ -29,3 +30,7 @@ class Dbscan:
         clusters_labels = dbscan.labels_
         df['cluster'] = clusters_labels
         return df
+    
+    def generate_csv(self)->None:
+        df = self.__get_results__()
+        df.to_csv('dbscan.csv', index=False)
